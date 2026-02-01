dd009abee157e8e19bb6c7934c1ef629cde5d903
kartik428 <emaildeleted444@gmail.com>
2026-02-01 13:04:49 +0530

    final vodeo compressed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         className="absolute top-0 left-0 w-full h-screen object-cover -z-10"
       >
         <source
-          src="/bgvideo.mp4"
+          src="/bgvideo2.mp4"
           type="video/mp4"
         />
       </video>
