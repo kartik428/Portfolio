d059bd30216bc9c0ac6627bccd992e4b400ee6b3
kartik428 <emaildeleted444@gmail.com>
2026-02-01 12:31:37 +0530

    same

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         className="absolute top-0 left-0 w-full h-screen object-cover -z-10"
       >
         <source
-          src="/15439679-uhd_2560_1440_30fps.mp4"
+          src="/bgvideo.mp4"
           type="video/mp4"
         />
       </video>
